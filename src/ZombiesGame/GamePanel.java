4f014fed390bcdd2a37c013f1be232f68ecaf174
Kyle Chen <kyle.chen@sjsu.edu>
2021-10-19 23:29:06 -0700

    added instance variables panelwidth and panelHeight to determine the Dimensions of the JPanel and initialized them in constructor

--- src/ZombiesGame/GamePanel.java
+++ src/ZombiesGame/GamePanel.java
@@ -2,13 +2,25 @@ package ZombiesGame;
 
 public class GamePanel
 {
-    private final int SCALE = 4;
-    private final int BASE_TILE_SIZE = 16;
-    private final int SCALE_TILE_SIZE = SCALE * BASE_TILE_SIZE;
+    private final int SCALE             = 4;
+    private final int BASE_TILE_SIZE    = 16;
+    private final int SCALED_TILE_SIZE  = SCALE * BASE_TILE_SIZE;
 
+    private int panelWidth;
+    private int panelHeight;
+
+
+    /**
+     * constructor for GamePanel class that should initialize panelWidth and panelHeight,
+     * which will be used to set up the preferred size of the panel
+     * @param width the width of the Panel in terms of the number of tiles
+     * @param height the height of the Panel in terms of the number of tiles
+     */
     public GamePanel(int width, int height)
     {
-        
+        panelWidth  = width * SCALED_TILE_SIZE;
+        panelHeight = height * SCALED_TILE_SIZE;
+
 
     }
 
