8f0759415a1bea7ce1fd971245c1bf2292ce7797
nhingn <vnhin57@gmail.com>
2021-10-20 00:28:28 -0700

    added level class

---
+++ src/Level.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Level {
+    int score;
+    String gameStatus;
+    int zombieCount;
+    boolean isWin;
+
+    public void addScore() {
+        // some code here
+    }
+
+    public void addZombies() {
+        // some code here
+    }
+
+    public int getCurrentScore() {
+        // some code here
+        return score;
+    }
+
+    public void generateZombies() {
+        // some code here
+    }
+
+    public void generateItem() {
+        // some code here
+    }
+
+    public void setLevel() {
+        // some code here
+    }
+}
