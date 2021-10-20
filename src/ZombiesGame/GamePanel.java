30a3c132e8c3f67d05ac99a63c917ada3b595aa1
Kyle Chen <kyle.chen@sjsu.edu>
2021-10-19 23:15:48 -0700

    created src folder and GamePanel class with constant variables that determine the scale of the visuals

---
+++ src/ZombiesGame/GamePanel.java
@@ -0,0 +1,16 @@
+package ZombiesGame;
+
+public class GamePanel
+{
+    private final int SCALE = 4;
+    private final int BASE_TILE_SIZE = 16;
+    private final int SCALE_TILE_SIZE = SCALE * BASE_TILE_SIZE;
+
+    public GamePanel(int width, int height)
+    {
+        
+
+    }
+
+
+}
