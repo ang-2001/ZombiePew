8335dd31531119ae745503a3bc558f8b357d92e4
Kyle Chen <kyle.chen@sjsu.edu>
2021-11-07 13:50:36 -0800

    created Model class, defined constructor, defined basic classes to be fully implemented later

---
+++ src/ZombiesGame/model/Model.java
@@ -0,0 +1,25 @@
+package ZombiesGame.model;
+
+public class Model
+{
+    //private Player player;
+    // manages game state and interactions
+    public Model()
+    {
+        //this.player = new Player();
+        // other gamedata created here
+    }
+
+
+    public void movePlayer(int dx, int dy)
+    {
+//        player.setX(dx);
+//        player,setY(dy);
+    }
+
+
+//    public Player getPlayer()
+//    {
+//        return player;
+//    }
+}
