7d341b7d32338394b0d8b13917aaff7f46ffafe5
David Sambilay <sambilay.david@gmail.com>
2021-11-07 22:02:58 -0800

    ZOM 18 createdValve Interface

--- src/ZombiesGame/controller/Controller.java
+++ src/ZombiesGame/controller/Controller.java
@@ -1,15 +1,18 @@
 package ZombiesGame.controller;
 
+import ZombiesGame.Message;
 import ZombiesGame.view.View;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 public class Controller
 {
-//    private BlockingQueue<Message> queue;
+    private BlockingQueue<Message> queue;
 //    private Model model;
     private View view;
-
+    private List<Valve> valves = new LinkedList<>();
     
     // add constructor arguments as needed
     //queue, model, view
@@ -22,6 +25,22 @@ public class Controller
 
     public void mainLoop() throws Exception
     {
-
+        ValveResponse response = ValveResponse.EXECUTED;
+        Message message = null;
+        while (response != ValveResponse.FINISH){
+            try {
+                message = queue.take();
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            for (Valve valve : valves) {
+                response = valve.execute(message);
+                // if successfully processed or game over, leave the loop
+                if (response != ValveResponse.MISS) {
+                    break;
+                }
+            }
+        }
     }
 }
