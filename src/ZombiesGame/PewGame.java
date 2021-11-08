3fd353868d9ba92ac1d87c2f8016a961af85769b
Kyle Chen <kyle.chen@sjsu.edu>
2021-11-07 22:16:29 -0800

    created instances of model, view, controller classes in main. added arguments to controller constructor

--- src/ZombiesGame/PewGame.java
+++ src/ZombiesGame/PewGame.java
@@ -1,5 +1,7 @@
 package ZombiesGame;
 
+import ZombiesGame.controller.Controller;
+import ZombiesGame.model.Model;
 import ZombiesGame.view.GameWindow;
 import ZombiesGame.view.View;
 
@@ -9,10 +11,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class PewGame
 {
     private static BlockingQueue<Message> queue = new LinkedBlockingQueue<Message>();
+    private static Controller controller;
+    private static Model model;
+    private static View view;
 
     public static void main(String[] args)
     {
-        View view = new View();
+        model = new Model();
+        view = new View();
+        controller = new Controller(queue, model, view);
+
         GameWindow window = new GameWindow("Pew Game", view);
     }
 }
